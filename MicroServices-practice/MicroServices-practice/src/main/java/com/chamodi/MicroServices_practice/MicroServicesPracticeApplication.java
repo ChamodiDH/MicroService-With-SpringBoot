27b2ab3b62f5667a1c7b13bacd79c3dca31378a3
ChamodiDH <chamodi3797@gmail.com>
2025-01-07 19:05:30 +0530

    application configurations for eureka server.

--- MicroServices-practice/MicroServices-practice/src/main/java/com/chamodi/MicroServices_practice/MicroServicesPracticeApplication.java
+++ MicroServices-practice/MicroServices-practice/src/main/java/com/chamodi/MicroServices_practice/MicroServicesPracticeApplication.java
@@ -2,8 +2,10 @@ package com.chamodi.MicroServices_practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // This application will run as the service registry.
 public class MicroServicesPracticeApplication {
 
 	public static void main(String[] args) {
