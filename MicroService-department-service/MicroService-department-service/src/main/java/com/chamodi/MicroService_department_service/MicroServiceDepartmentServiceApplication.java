0ec8d23c27c32bd53c6a61af9b7d992d02bdd22e
ChamodiDH <chamodi3797@gmail.com>
2025-01-07 19:29:58 +0530

    Initialize department micro service.

---
+++ MicroService-department-service/MicroService-department-service/src/main/java/com/chamodi/MicroService_department_service/MicroServiceDepartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chamodi.MicroService_department_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceDepartmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceDepartmentServiceApplication.class, args);
+	}
+
+}
---
+++ MicroService-department-service/MicroService-department-service/src/test/java/com/chamodi/MicroService_department_service/MicroServiceDepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamodi.MicroService_department_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceDepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
