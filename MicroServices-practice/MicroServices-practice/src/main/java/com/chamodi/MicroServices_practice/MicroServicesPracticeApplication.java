b44493ef5441df33838dc9fb4d943c9f12504b91
ChamodiDH <chamodi3797@gmail.com>
2025-01-07 18:20:19 +0530

    Intitialize the project with dependencies

---
+++ MicroServices-practice/MicroServices-practice/src/main/java/com/chamodi/MicroServices_practice/MicroServicesPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.chamodi.MicroServices_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicesPracticeApplication.class, args);
+	}
+
+}
---
+++ MicroServices-practice/MicroServices-practice/src/test/java/com/chamodi/MicroServices_practice/MicroServicesPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamodi.MicroServices_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicesPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
