92f48ceb2199b19d18a69465fa3420333e7a4b44
ChamodiDH <chamodi3797@gmail.com>
2025-01-08 16:29:54 +0530

    Add Config server with configs.

---
+++ MicroService-config-server/MicroService-config-server/src/test/java/com/chamodi/MicroService_config_server/MicroServiceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamodi.MicroService_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
